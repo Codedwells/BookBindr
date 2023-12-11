7ca3febdba9b6ea6536c224b3e16ea3d4d7e7f5d
codedwells <wells.mail.13@gmail.com>
2023-12-11 23:02:49 +0300

    feat(recommend): Recommend more books

---
+++ server/books.js
@@ -0,0 +1,148 @@
+export const books = [
+  {
+    "title": "To Kill a Mockingbird",
+    "link": "https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird",
+    "scores": [95, 80, 60, 70]
+  },
+  {
+    "title": "The Hitchhiker's Guide to the Galaxy",
+    "link": "https://en.wikipedia.org/wiki/The_Hitchhiker%27s_Guide_to_the_Galaxy",
+    "scores": [60, 90, 80, 95]
+  },
+  {
+    "title": "1984",
+    "link": "https://en.wikipedia.org/wiki/Nineteen_Eighty-Four",
+    "scores": [90, 70, 50, 60]
+  },
+  {
+    "title": "The Great Gatsby",
+    "link": "https://en.wikipedia.org/wiki/The_Great_Gatsby",
+    "scores": [80, 75, 55, 65]
+  },
+  {
+    "title": "Brave New World",
+    "link": "https://en.wikipedia.org/wiki/Brave_New_World",
+    "scores": [85, 60, 75, 50]
+  },
+  {
+    "title": "The Lord of the Rings",
+    "link": "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings",
+    "scores": [70, 95, 85, 50]
+  },
+  {
+    "title": "Pride and Prejudice",
+    "link": "https://en.wikipedia.org/wiki/Pride_and_Prejudice",
+    "scores": [75, 50, 55, 80]
+  },
+  {
+    "title": "The Catcher in the Rye",
+    "link": "https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye",
+    "scores": [65, 55, 60, 75]
+  },
+  {
+    "title": "Harry Potter and the Sorcerer's Stone",
+    "link": "https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone",
+    "scores": [75, 85, 90, 70]
+  },
+  {
+    "title": "The Shining",
+    "link": "https://en.wikipedia.org/wiki/The_Shining_(novel)",
+    "scores": [85, 70, 55, 75]
+  },
+  {
+    "title": "The Hobbit",
+    "link": "https://en.wikipedia.org/wiki/The_Hobbit",
+    "scores": [60, 90, 80, 70]
+  },
+  {
+    "title": "The Da Vinci Code",
+    "link": "https://en.wikipedia.org/wiki/The_Da_Vinci_Code",
+    "scores": [70, 75, 65, 85]
+  },
+  {
+    "title": "Frankenstein",
+    "link": "https://en.wikipedia.org/wiki/Frankenstein",
+    "scores": [85, 55, 60, 70]
+  },
+  {
+    "title": "The Great Expectations",
+    "link": "https://en.wikipedia.org/wiki/Great_Expectations",
+    "scores": [80, 50, 65, 75]
+  },
+  {
+    "title": "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+    "link": "https://en.wikipedia.org/wiki/The_Lion,_the_Witch_and_the_Wardrobe",
+    "scores": [70, 85, 80, 65]
+  },
+  {
+    "title": "Fahrenheit 451",
+    "link": "https://en.wikipedia.org/wiki/Fahrenheit_451",
+    "scores": [90, 60, 55, 70]
+  },
+  {
+    "title": "Moby-Dick",
+    "link": "https://en.wikipedia.org/wiki/Moby-Dick",
+    "scores": [75, 70, 50, 65]
+  },
+  {
+    "title": "Alice's Adventures in Wonderland",
+    "link": "https://en.wikipedia.org/wiki/Alice%27s_Adventures_in_Wonderland",
+    "scores": [60, 80, 85, 75]
+  },
+  {
+    "title": "The War of the Worlds",
+    "link": "https://en.wikipedia.org/wiki/The_War_of_the_Worlds",
+    "scores": [55, 95, 80, 65]
+  },
+  {
+    "title": "The Count of Monte Cristo",
+    "link": "https://en.wikipedia.org/wiki/The_Count_of_Monte_Cristo",
+    "scores": [80, 65, 55, 75]
+  },
+  {
+    "title": "The Jungle Book",
+    "link": "https://en.wikipedia.org/wiki/The_Jungle_Book",
+    "scores": [60, 85, 75, 70]
+  },
+  {
+    "title": "The Picture of Dorian Gray",
+    "link": "https://en.wikipedia.org/wiki/The_Picture_of_Dorian_Gray",
+    "scores": [85, 60, 65, 70]
+  },
+  {
+    "title": "The Girl with the Dragon Tattoo",
+    "link": "https://en.wikipedia.org/wiki/The_Girl_with_the_Dragon_Tattoo",
+    "scores": [70, 75, 90, 65]
+  },
+  {
+    "title": "A Tale of Two Cities",
+    "link": "https://en.wikipedia.org/wiki/A_Tale_of_Two_Cities",
+    "scores": [75, 50, 55, 80]
+  },
+  {
+    "title": "The Alchemist",
+    "link": "https://en.wikipedia.org/wiki/The_Alchemist_(novel)",
+    "scores": [65, 80, 90, 70]
+  },
+  {
+    "title": "Wuthering Heights",
+    "link": "https://en.wikipedia.org/wiki/Wuthering_Heights",
+    "scores": [80, 55, 60, 70]
+  },
+  {
+    "title": "The Odyssey",
+    "link": "https://en.wikipedia.org/wiki/Odyssey",
+    "scores": [70, 90, 80, 60]
+  },
+  {
+    "title": "Les Misérables",
+    "link": "https://en.wikipedia.org/wiki/Les_Mis%C3%A9rables",
+    "scores": [75, 50, 55, 80]
+  },
+  {
+    "title": "The Brothers Karamazov",
+    "link": "https://en.wikipedia.org/wiki/The_Brothers_Karamazov",
+    "scores": [85, 60, 70, 55]
+  }
+]
+
--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import cors from 'cors'
-import {Borgen, Logger } from 'borgen'
+import { Borgen, Logger } from 'borgen'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { books } from './books.js'
 
 const PORT = 6969
 
@@ -19,60 +20,6 @@ const genres = [
     { name: 'Comedy', score: 65 },
 ]
 
-// Simple in-memory db
-const books = [
-    {
-        title: 'To Kill a Mockingbird',
-        link: 'https://en.wikipedia.org/wiki/To_Kill_a_Mockingbird',
-        scores: [95, 80, 60, 70], // Drama, Adventure, SciFi, Comedy
-    },
-    {
-        title: "The Hitchhiker's Guide to the Galaxy",
-        link: 'https://en.wikipedia.org/wiki/The_Hitchhiker%27s_Guide_to_the_Galaxy',
-        scores: [60, 90, 80, 95],
-    },
-    {
-        title: '1984',
-        link: 'https://en.wikipedia.org/wiki/Nineteen_Eighty-Four',
-        scores: [90, 70, 50, 60],
-    },
-    {
-        title: 'The Great Gatsby',
-        link: 'https://en.wikipedia.org/wiki/The_Great_Gatsby',
-        scores: [80, 75, 55, 65],
-    },
-    {
-        title: 'Brave New World',
-        link: 'https://en.wikipedia.org/wiki/Brave_New_World',
-        scores: [85, 60, 75, 50],
-    },
-    {
-        title: 'The Lord of the Rings',
-        link: 'https://en.wikipedia.org/wiki/The_Lord_of_the_Rings',
-        scores: [70, 95, 85, 50],
-    },
-    {
-        title: 'Pride and Prejudice',
-        link: 'https://en.wikipedia.org/wiki/Pride_and_Prejudice',
-        scores: [75, 50, 55, 80],
-    },
-    {
-        title: 'The Catcher in the Rye',
-        link: 'https://en.wikipedia.org/wiki/The_Catcher_in_the_Rye',
-        scores: [65, 55, 60, 75],
-    },
-    {
-        title: "Harry Potter and the Sorcerer's Stone",
-        link: 'https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone',
-        scores: [75, 85, 90, 70],
-    },
-    {
-        title: 'The Shining',
-        link: 'https://en.wikipedia.org/wiki/The_Shining_(novel)',
-        scores: [85, 70, 55, 75],
-    },
-]
-
 // Vector difference func
 function vectorDifference(user, book) {
     let diff = 0
@@ -83,27 +30,42 @@ function vectorDifference(user, book) {
 }
 
 app.get('/recommend', (req, res) => {
-    let { genre } = req.query
-    let userPrefs = genres.map((g) => (g.name === genre ? g.score : 0))
+    try {
+        let { genre } = req.query
+        let userPrefs = genres.map((g) => (g.name === genre ? g.score : 0))
 
-    // Best match
-    let minDiff = Infinity
-    let bestMatch
+        // Find top four matches
+        let matches = []
+        for (let i = 0; i < 4; i++) {
+            let minDiff = Infinity
+            let bestMatch
 
-    books.forEach((book) => {
-        let diff = vectorDifference(userPrefs, book)
-        if (diff < minDiff) {
-            minDiff = diff
-            bestMatch = book
+            books.forEach((book) => {
+                // Exclude books that are already in the result
+                if (matches.some((match) => match.title === book.title)) {
+                    return
+                }
+
+                let diff = vectorDifference(userPrefs, book)
+                if (diff < minDiff) {
+                    minDiff = diff
+                    bestMatch = book
+                }
+            })
+
+            // Add the best match to the result
+            matches.push({
+                title: bestMatch.title,
+                link: bestMatch.link,
+            })
         }
-    })
 
-    res.status(200).json([
-        {
-            title: bestMatch.title,
-            link: bestMatch.link,
-        }
-    ])
+        res.status(200).json(matches)
+    } catch (err) {
+        res.status(500).json({
+            message: 'An error occured while fetching data',
+        })
+    }
 })
 
 app.listen(PORT, () => {
